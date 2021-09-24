ea5935bb65409308185fa05e62fb772bc0339d0d
VadimasKo <vadim.kozemiakin@ntt.lt>
2021-09-24 16:57:59 +0300

    Plan outline && add git ignore

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <string>
+#include <bitset>
+#include <bit>
+
+using namespace std;
+
+
+/* Plan:
+  1) Divide string into 64 char sections
+  2) Convert Sections into binary strings
+  3) (Optional) Shift binary values
+  4) Merge Sections using AND gate
+  5) (Optional) Shift binary balues
+  6) Convert binary string into HEX string
+
+  Later: 
+  1) Create userInterface
+  2) Allow passing props by txt && then calling function
+  3) Measure
+  4+) BLA BLA BLA
+*/
+
 
 
 int main() {
-  std::cout<<"initial";
+  cout<<"ODA_SOD_ASAP+DSAPD+SA_DSA-D"
 }
