d21d374bd0c1e659271ea4d5daf4358f1fa45ef3
VadimasKo <vadim.kozemiakin@ntt.lt>
2021-10-03 18:10:42 +0300

    compare durration with md5

--- LineTest.cpp
+++ LineTest.cpp
@@ -2,9 +2,45 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <chrono>
 
 using namespace std;
+using highResClock =   chrono::high_resolution_clock;
+using timePoint =      chrono::time_point<highResClock>;
+using durationDouble = chrono::duration<double>;
+class Timer {
+  private: 
+    timePoint start;
 
+  public:
+    void startClock() {
+      start = highResClock::now();
+    }
+
+    double calcDuration() {
+      durationDouble duration = highResClock::now() - start;
+      return duration.count();
+    }
+};
+
+int callMd5(string line) {
+  system(("echo -n " +line + " | md5sum").c_str());
+  return 1;
+}
+
+double runTests(string command, string fileString) {
+  stringstream fileStream(fileString);
+  Timer timer;
+  timer.startClock();
+  string lineValue;
+  while(!fileStream.eof()) {
+    getline(fileStream, lineValue);
+    lineValue.insert(lineValue.begin(),'\"');
+    lineValue.push_back('\"');
+    (command == "md5sum") ? callMd5(lineValue) : system((command + lineValue).c_str());
+  }
+  return timer.calcDuration();
+}
 
 
 int main(int argc, char *argv[]) {
@@ -15,13 +51,13 @@ int main(int argc, char *argv[]) {
   file.open(fileLoc);
   fileStream<<file.rdbuf();
   file.close();
-  string lineValue;
-  // while(!fileStream.eof()){
-    getline(fileStream, lineValue);
-    // const char *myFuncCall = ("time ./HashGenerator test " + lineValue).c_str();
-    // system(myFuncCall);
-    system(("./HashGenerator "+ lineValue).c_str());
-    cout<<"abc"<<endl;
-  // }
+  string fileString = fileStream.str();
 
+  double myDuration = runTests("./HashGenerator test ", fileString);
+  double md5Duration = runTests("md5sum", fileString);
+  system("clear");
+  cout<<"Duration of mine hash generator "<<myDuration<<"\n";
+  cout<<"Duration of md5 hash generator "<<md5Duration<<"\n"; 
 }
+
+double RunTests(string command, stringstream filesream);
