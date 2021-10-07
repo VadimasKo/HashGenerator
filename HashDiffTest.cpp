2acd1a20df51e613f20e43fe7bf94c54e2e4af2e
VadimasKo <vadim.kozemiakin@ntt.lt>
2021-10-07 22:46:22 +0300

    read pairs

--- HashDiffTest.cpp
+++ HashDiffTest.cpp
@@ -1,29 +1,57 @@
 #include <iostream>
-#include <array>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
 
 using namespace std;
 
-void pipeCommand(string strCmd) {
-  array<char, 80> buffer;
-  FILE *pipe = popen(strCmd.c_str(), "r");
-  if (!pipe) {
-    cerr << "cannot open pipe to read \n";
-    return;
-  }
-  int c=0;
-  while(fgets(buffer.data(), 80, pipe) != NULL) {
-      c++;
-      std::cout<<c<<" "<< buffer.data();
-  }
-  pclose(pipe);
-} 
+string pipeCommand(string command);
+vector<string> getPairs(string filePath);
 
 int main() {
-  string strCmd = "";
-  while(1) {
-    cout<<"enter a command \n";
-    getline(cin, strCmd);
-    pipeCommand(strCmd);
-  }
+  string command = "./HashGenerator.exe ./RandomTestFiles/10.txt";
+  string buffer = pipeCommand(command);
+  cout<<buffer.length()<<buffer;
+  getPairs("./RandomTestFiles/10.txt");
   return 0;
 }
+
+
+vector<string> getPairs(string filePath) {
+  vector<string> pairs;
+  stringstream fileStream;
+  ifstream file;
+  file.open(filePath);
+  fileStream<<file.rdbuf();
+  file.close();
+
+  
+  while(!fileStream.eof()) {
+    string line;
+    string pair;
+    getline(fileStream, line);
+    stringstream lineStream(line);
+    lineStream>>pair;
+    pairs.push_back(pair);
+    cout<<pair<<'\t';
+    lineStream>>pair;
+    cout<<pair<<'\n';
+    pairs.push_back(pair);
+  }
+
+  return pairs;
+}
+
+
+string pipeCommand(string command) {
+  FILE *pipe = popen(command.c_str(), "r");
+  string result;
+  char line[64];
+ 
+  while (fgets(line, 64, pipe)) {
+    result += line;
+  }
+  pclose(pipe);
+  return result;
+} 
