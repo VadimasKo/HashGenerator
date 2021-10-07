b33c4c5d2102ec95c3e9ba35b020b09e167bbe72
VadimasKo <vadim.kozemiakin@ntt.lt>
2021-10-08 00:59:29 +0300

    hash dif testing

--- GenerateRandom.cpp
+++ GenerateRandom.cpp
@@ -16,7 +16,7 @@ class RandInt{
       int operator ()() {return dist(mt);}
 };
 
-RandInt rnd {48,122};
+RandInt rnd {48,91};
 
 void generateUniquePairs(int size, string fileName) {
   ofstream file;
